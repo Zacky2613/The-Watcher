ad6ba98445dcb1d5b01174ed3a9c45c4e12ad05c
Zacky2613 <zackycuber@gmail.com>
2023-03-07 22:40:50 +1000

    Fixed a bot token accidental leak

--- src/main.py
+++ src/main.py
@@ -12,10 +12,8 @@ bot.remove_command("help")
 blacklist_data = []
 server_data = {}
 
-server_db = 1050367619938648084
-# server_db = int(os.environ["server_db"])
-blacklist_db = 1050367648493473877
-# blacklist_db = int(os.environ["blacklist_db"])
+server_db = int(os.environ["server_db"])
+blacklist_db = int(os.environ["blacklist_db"])
 
 
 with open("./Json/words.json", "r", encoding="utf-8") as f:
@@ -196,5 +194,4 @@ async def on_message(ctx):
     report_channel, alert_ping = await getreportchannel(ctx)
     await slurfilter.slur_filter(ctx=ctx, data=(word_data, blacklist_data, report_channel, alert_ping))
 
-# bot.run(os.environ["DISCORD_TOKEN"])
-bot.run("MTAwMjgzMTgzNzY1NzMxNzQyNw.GvAD4q.1fte_3zk6RspfN6X0lHgqPoRV2SKnASal7VC2M")
+bot.run(os.environ["DISCORD_TOKEN"])
